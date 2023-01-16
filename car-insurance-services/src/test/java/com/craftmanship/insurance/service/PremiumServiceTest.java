f7efe150b151dcf2a44dca4da459928ebbeba4c5
Katrin Heiderer <katrin.heiderer@nagarro.com>
2023-01-16 17:19:28 +0100

    add: Add new Unit Test instead of Integration Tests

--- car-insurance-services/src/test/java/com/craftmanship/insurance/service/PremiumServiceTest.java
+++ car-insurance-services/src/test/java/com/craftmanship/insurance/service/PremiumServiceTest.java
@@ -1,16 +1,9 @@
 package com.craftmanship.insurance.service;
 
-import com.craftmanship.insurance.InsuranceServicesApplication;
 import com.craftmanship.insurance.entities.Coverage;
 import com.craftmanship.insurance.model.PremiumRequestDTO;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
 
@@ -22,15 +15,65 @@ class PremiumServiceTest {
     public static final int STANDARD_POWER = 100;
     public static final int BONUS_MALUS_LEVEL = 9;
 
-   @Test
-    public void calculatePremiumWithDifferentBonusMalusLevels() {
+    @ParameterizedTest
+    @CsvSource({"0, 44.00",
+            "1, 44.00",
+            "2, 52.80",
+            "3, 52.80",
+            "4, 61.60",
+            "5, 61.60",
+            "6, 70.40",
+            "8, 88.00",
+            "9, 88.00",
+            "10, 105.60",
+            "11, 105.60",
+            "12, 123.20",
+            "13, 123.20",
+            "14, 149.60",
+            "15, 149.60",
+            "16, 176.00",
+            "17, 176.00"})
+    public void calculatePremiumWithDifferentBonusMalusLevels(int bonusMalusLevel, String expectedPremium) {
+        PremiumRequestDTO input = new PremiumRequestDTO(STANDARD_POWER, bonusMalusLevel, NO_RISK_ZIP_CODE, 1L);
+
+        Coverage coverage = new Coverage();
+        coverage.setPercentagePremium(new BigDecimal("0.88"));
+        coverage.setMaxPremium(new BigDecimal("132"));
+        coverage.setMinPremium(new BigDecimal("23.76"));
+
+        assertThat(new PremiumService().calculatePremium(input, coverage)).isEqualTo(new BigDecimal(expectedPremium));
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"10, 23.80",
+            "27, 23.80",
+            "28, 24.70",
+            "146, 128.50",
+            "147, 132.00",
+            "200, 132.00"})
     public void calculatePremiumWithDifferentPowerRanges(int power, String expectedPremium) {
+        PremiumRequestDTO input = new PremiumRequestDTO(power, BONUS_MALUS_LEVEL, NO_RISK_ZIP_CODE, 1L);
+
+        Coverage coverage = new Coverage();
+        coverage.setPercentagePremium(new BigDecimal("0.88"));
+        coverage.setMaxPremium(new BigDecimal("132"));
+        coverage.setMinPremium(new BigDecimal("23.76"));
+        assertThat(new PremiumService().calculatePremium(input, coverage)).isEqualTo(new BigDecimal(expectedPremium));
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"1000, 83.60",
+            "3333, 83.60",
+            "3334, 88.00",
+            "6666, 88.00",
+            "6667, 92.40",
+            "9999, 92.40"})
     public void calculatePremiumWithDifferentRiskLocations(int zipCode, String expectedPremium) {
+        PremiumRequestDTO input = new PremiumRequestDTO(STANDARD_POWER, BONUS_MALUS_LEVEL, zipCode, 1L);
+        Coverage coverage = new Coverage();
+        coverage.setPercentagePremium(new BigDecimal("0.88"));
+        coverage.setMaxPremium(new BigDecimal("132"));
+        coverage.setMinPremium(new BigDecimal("23.76"));
+        assertThat(new PremiumService().calculatePremium(input, coverage)).isEqualTo(new BigDecimal(expectedPremium));
     }
 }
--- car-insurance-services/src/test/java/com/craftmanship/insurance/service/TaxServiceTest.java
+++ car-insurance-services/src/test/java/com/craftmanship/insurance/service/TaxServiceTest.java
@@ -20,17 +20,42 @@ class TaxServiceTest {
 
     @Test
     public void calculateTaxForElectric() {
+
+        TaxRequestDTO input = new TaxRequestDTO(100, 100, "electricity", LocalDate.of(2022, 4, 21));
+
+        assertThat(new TaxService().calculateTax(input)).isEqualTo(new BigDecimal("0.00"));
     }
 
-    @Test
-    public void calculateTaxForGasoline() {
+    @ParameterizedTest
+    @CsvSource({"165, 215, 144.00",
+            "110, 135, 46.80",
+            "65, 115, 7.20",})
+    public void calculateTaxForGasoline(int power, int co2Emissions, String expectedTax) {
+
+        TaxRequestDTO input = new TaxRequestDTO(co2Emissions, power, "gasoline", LocalDate.of(2022, 4, 21));
+
+        assertThat(new TaxService().calculateTax(input)).isEqualTo(new BigDecimal(expectedTax));
     }
 
-    @Test
-    public void calculateTaxForDiesel() {
+    @ParameterizedTest
+    @CsvSource({"165, 215, 144.00",
+            "110, 135, 46.80",
+            "65, 115, 7.20",})
+    public void calculateTaxForDiesel(int power, int co2Emissions, String expectedTax) {
+
+        TaxRequestDTO input = new TaxRequestDTO(co2Emissions, power, "diesel", LocalDate.of(2022, 4, 21));
+
+        assertThat(new TaxService().calculateTax(input)).isEqualTo(new BigDecimal(expectedTax));
     }
 
-    @Test
-    public void calculateTaxForHybrid() {
+    @ParameterizedTest
+    @CsvSource({"165, 215, 72.00",
+            "110, 135, 32.40",
+            "65, 115, 3.60",})
+    public void calculateTaxForHybrid(int power, int co2Emissions, String expectedTax) {
+
+        TaxRequestDTO input = new TaxRequestDTO(co2Emissions, power, "hybrid", LocalDate.of(2022, 4, 21));
+
+        assertThat(new TaxService().calculateTax(input)).isEqualTo(new BigDecimal(expectedTax));
     }
 }
