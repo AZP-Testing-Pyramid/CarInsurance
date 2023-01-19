5a8f260ef349a578a6167e769729ea9e6fd7ff9b
Daniel Gomez-Sanchez <daniel.gomez-sanchez@allianz.at>
2023-01-19 16:49:41 +0100

    Add more test cases to TaxCalculationIntegrationTest

--- car-insurance-services/src/test/java/com/craftmanship/insurance/integration/TaxCalculationIntegrationTest.java
+++ car-insurance-services/src/test/java/com/craftmanship/insurance/integration/TaxCalculationIntegrationTest.java
@@ -7,10 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -34,11 +39,52 @@ public class TaxCalculationIntegrationTest {
     }
 
     @Test
-    public void shouldBecomeAUsefulTest() {
-    	ResponseEntity<TaxResponseDTO> res = restTemplate.postForEntity(createURLWithPort("/tax"), //
-    			new TaxRequestDTO(0, 200, "electricity", LocalDate.now()), //
-    			TaxResponseDTO.class);
-    	Assertions.assertThat(res.getBody().tax()).isEqualTo(BigDecimal.ZERO.setScale(2));
+    public void testElectric() {
+    	testTaxCalculation(0, 200, "electricity", "0.00");
     }
+
+    @CsvSource({
+    	"100,25.20",
+    	"200,97.20",
+    	"69,3.60",
+    	"70,3.60"    	
+    })
+    @ParameterizedTest
+    public void testHybrid(final int power, final String result) {
+    	testTaxCalculation(0, power, "hybrid", result);
+    }
+    
+    @MethodSource("provideCo2RelevantCases")
+    @ParameterizedTest
+    public void testGasoline(final int co2, final int power, final String result) {
+    	testTaxCalculation(co2, power, "gasoline", result);
+    }
+    
+    @MethodSource("provideCo2RelevantCases")
+    @ParameterizedTest
+    public void testDiesel(final int co2, final int power, final String result) {
+    	testTaxCalculation(co2, power, "diesel", result);
+    }
+
+	private void testTaxCalculation(int co2Emissions, int power, final String fuelType, final String result) {
+		TaxRequestDTO request = new TaxRequestDTO(co2Emissions, power, fuelType, LocalDate.now());
+		
+		ResponseEntity<TaxResponseDTO> res = restTemplate.postForEntity(createURLWithPort("/tax"), //
+				request, //
+    			TaxResponseDTO.class);
+		
+    	Assertions.assertThat(res.getBody().tax()).isEqualTo(new BigDecimal(result));
+	}
+    
+	private static Stream<Arguments> provideCo2RelevantCases() {
+	    return Stream.of(
+	      Arguments.of(120, 100, "28.80"),
+	      Arguments.of(119, 200,"100.80"),
+	      Arguments.of(200, 69, "64.80"),
+	      Arguments.of(200, 70, "64.80"),
+	      Arguments.of(120, 69, "7.20"),
+	      Arguments.of(119, 70, "7.20")
+	    );
+	} 
 
 }
